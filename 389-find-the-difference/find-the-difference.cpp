f8d729b092fd283e48670844820b254e66e15bf3
abhisheksingh452 <abhisheksingh75775@gmail.com>
2026-08-24 10:18:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (8.43%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>freq;
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c:t){
+            freq[c]--;
+            if(freq[c]<0){
+                return c;
+            }
+        }
+
+        return ' ';
+    }
+};
