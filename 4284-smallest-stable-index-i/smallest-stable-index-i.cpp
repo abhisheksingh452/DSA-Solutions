69698a27ef729656e7991517f22cca0ffddae09e
Abhishek Kumar Singh <abhisheksingh75775@gmail.com>
2026-09-05 13:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (56.51%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-       int mx=0;
-       for(int i=0;i<nums.size();i++){
+       int n =nums.size();
+       vector<int>minel(n);
+       minel[n-1]=nums[n-1];
+       for(int i=n-2;i>=0;i--){
+        minel[i]=min(minel[i+1],nums[i]);
+       }
+
+       int mx=nums[0];
+       for(int i=0;i<n;i++){
         mx=max(mx,nums[i]);
-        int minele=*min_element(nums.begin()+i,nums.end());
-        int si=(mx-minele);
-        if(si<=k){
-            return i;
-        }
-       } 
+        int si=(mx-minel[i]);
+        if(si<=k)return i;
+       }
        return -1;
     }
 };
